5b1fdd433b9ad7b8b9621a4ec5c803c25572e6c9
weinge <wein.geoffrey@gmail.com>
2021-07-09 21:30:01 -0500

    Created questions, created inquirer prompts and logic to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,95 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "projectTitle",
+        message: "What is the title of your project?",
+        type: "input",
+    },
+    {
+        name: "projectDescription",
+        message: "Enter a description for your project:",
+        type: "input",
+    },
+    {
+        name: "installConfirm",
+        message: "Does your project require any installation instructions?",
+        type: "confirm",
+        default: "true",
+    },
+    {
+        name: "installInstruction",
+        message: "Enter your installation instructions:",
+        type: "input",
+        when: answers => !!answers.installConfirm,
+    },
+    {
+        name: "usageInfo",
+        message: "Enter how to use your project:",
+        type: "input",
+    },
+    {
+        name: "contributionConfirm",
+        message: "Are there any contribution guidelines?",
+        type: "confirm",
+        default: "true",
+    },
+    {
+        name: "contributionGuide",
+        message: "Enter the contribution guidelines (leave blank for Contributor Covenant Code of Conduct):",
+        type: "input",
+        when: answers => !!answers.contributionConfirm,
+    },
+    {
+        name: "testingConfirm",
+        message: "Are there any testing instructions?",
+        type: "confirm",
+        default: "true",
+    },
+    {
+        name: "testingInstructions",
+        message: "Enter testing instructions:",
+        type: "input",
+        when: answers => !!answers.testingConfirm,
+    },
+    {
+        name: "licenseSelect",
+        message: "Select a license from the following list:",
+        type: "list",
+        choices: [
+            "Apache",
+            "GNU GPLv2",
+            "GNU GPLv3",
+            "MIT",
+            "ISC",
+            "No License",
+        ],
+    },
+    {
+        name: "githubUsername",
+        message: "Enter your GitHub username:",
+        type: "input",
+    },
+    {
+        name: "emailAddress",
+        message: "Enter your email address (for questions relating to your project):",
+        type: "input",
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        generateMarkdown(answers)
+    });
+}
 
 // Function call to initialize app
 init();
